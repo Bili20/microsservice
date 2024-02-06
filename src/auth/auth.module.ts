f8f3b98b8122737a6cd34ab0ff85afbb63e38a1a
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-02-06 17:48:09 -0300

    Update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Person } from 'src/person/entities/person.entity';
+import { PersonService } from 'src/person/person.service';
+import { AuthGuard } from './auth.guard';
+@Global()
 @Module({
   imports: [
     JwtModule.register({
+      global: true,
       secret: '123456',
       signOptions: { expiresIn: '1h' },
     }),
+    TypeOrmModule.forFeature([Person]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
+  exports: [AuthGuard],
 })
 export class AuthModule {}
