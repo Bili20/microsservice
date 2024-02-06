a9020433d9138c8f67961bbbf62f25e84f9607dd
gabriel milosz cruz <gbrlmilosz@gmail.com>
2024-02-06 17:48:11 -0300

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Person } from 'src/person/entities/person.entity';
+import { PersonService } from 'src/person/person.service';
+import { Repository } from 'typeorm';
+import { AuthDto } from './dto/creat-auth.dto';
+const bcrypt = require('bcrypt');
 @Injectable()
 export class AuthService {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    @InjectRepository(Person) private personRepo: Repository<Person>,
+  ) {}
 
-  login(username: string, password: string) {}
+  async login({ email, password }: AuthDto) {
+    const person = await this.personRepo.findOne({ where: { email: email } });
+
+    if (person) {
+      const verifyPassword = bcrypt.compareSync(password, person.password);
+      if (verifyPassword) {
+        const payload = { sub: person.id, user: person.email };
+        return {
+          access_token: this.jwtService.sign(payload),
+        };
+      }
+    }
+
+    throw new UnauthorizedException();
+  }
 }
